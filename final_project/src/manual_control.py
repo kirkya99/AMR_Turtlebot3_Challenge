d8fa7e8639d6545accf6ffe8bd61c98277c67a0b
fs-213804 <fs-213804@rwu.de>
2023-11-22 00:37:36 +0100

    added comments to manual_control. #29

--- final_project/src/manual_control.py
+++ final_project/src/manual_control.py
@@ -7,6 +7,7 @@ from geometry_msgs.msg import Twist, PoseWithCovarianceStamped
 
 
 class ManualControl:
+    # Initializes the ManualControl class.
     def __init__(self):
         self.infinity = 2.0
         self.move_base_controller = move_base_controller.MovebaseController()
@@ -26,9 +27,8 @@ class ManualControl:
         self.STATUS = self.FORWARD
         self.sensor_max_range = self.infinity
 
-        
+    # Callback function for the laser scan distance values.
     def laser_scan_callback(self, laser_scan):
-        # self.min_distance_front = min(laser_scan.ranges[-45:45])
         self.front = laser_scan.ranges[0]
         self.left_front = laser_scan.ranges[75]
         self.left = laser_scan.ranges[90]
@@ -50,34 +50,38 @@ class ManualControl:
         if self.right_aft > self.infinity or self.right_aft > self.sensor_max_range:
             self.right_aft = self.sensor_max_range
 
+    # Callback function for the determination of the current position by amcl_pose.
     def amcl_pose_callback(self, msg):
         self.position = msg.pose.pose.position
         self.orientation = msg.pose.pose.orientation
 
+    # Moves the robot forward with the speed of 0.1 m/s.
     def move_forward(self):
         self.twist.linear.x = 0.1
         self.twist.angular.z = 0.0
 
+    # Turns the robot right with the speed of 0.25 m/s.
     def turn_right(self):
         self.twist.linear.x = 0.0
         self.twist.angular.z = -0.25
 
+    # Stops the angular and linear movements of the robot.
     def stop_robot(self):
         self.twist.linear.x = 0.0
         self.twist.angular.z = 0.0
 
-    def enter_hard_zone(self, point_four, point_six):
+    # This function has the task of moving into the hard zone in three steps.
+    # 1. The robot navigates to an helper goal directly infront of the sixth goal.
+    # 2. The robot uses the orientation from amcl_pose to determinate if the goal is directly in front of it.
+    #    If not, the robot will turn right until the goal is directly in front of it.
+    # 3. The robot will move forward until the sixth goal is reached. The control is then returned to the goals_list.
+    def enter_hard_zone(self, point_six):
         rospy.Subscriber("/scan", LaserScan, self.laser_scan_callback)
         rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self.amcl_pose_callback)
 
         rospy.loginfo("[ManualControl] Waiting for laser scan data...")
         while self.front is None:
             rospy.sleep(0.1)
-
-        rospy.loginfo("[ManualControl] -----")
-        rospy.loginfo("[ManualControl] Point 4: x={}; y={}".format(point_four.x,point_four.y))
-        rospy.loginfo("[ManualControl] Point 6: x={}; y={}".format(point_six.x, point_six.y))
-        rospy.loginfo("[ManualControl] -----")
 
         x = 1.4726425599703223
         y = -0.42066028037912384
@@ -95,33 +99,29 @@ class ManualControl:
             status = self.move_base_controller.move_into_hard_zone()
 
         # Move forward until point six is reached
-        # rospy.loginfo("[ManualControl] Move forward until the end point is reached")
+        while True:
+            if self.check_orientation():
+                self.stop_robot()
+                break
+            else:
+                self.turn_right()
 
-        self.move_base_controller.move_base(self.end_point)
+            self.vel_pub.publish(self.twist)
 
-        # while True:
-        #     if self.check_orientation():
-        #         self.stop_robot()
-        #         break
-        #     else:
-        #         self.turn_right()
+        while True:
+            if self.position.x < self.end_point.x:
+                self.move_forward()
+            else:
+                self.stop_robot()
+                self.STATUS = self.IDLE
+                break 
 
-        #     self.vel_pub.publish(self.twist)
-
-        # while True:
-        #     if self.position.x < self.end_point.x:
-        #         self.move_forward()
-        #     else:
-        #         self.stop_robot()
-        #         self.STATUS = self.IDLE
-        #         break 
-
-        #     self.vel_pub.publish(self.twist)
+            self.vel_pub.publish(self.twist)
 
 
         rospy.loginfo("[ManualControl] End point is reached. Returning control to Main")
 
-
+    # This goal checks if the orientation fits with the sixth goal. If the goal is directly in front of it, the robot will return true and if not
     def check_orientation(self):
         if self.orientation.w == 1:
             return True
