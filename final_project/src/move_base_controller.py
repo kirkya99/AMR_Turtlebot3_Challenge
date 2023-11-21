cce46350af08399ec96d22ec7fc689c22ea29ef2
fs-213804 <fs-213804@rwu.de>
2023-11-22 00:42:07 +0100

    added comments to move_base_controller.py #29

--- final_project/src/move_base_controller.py
+++ final_project/src/move_base_controller.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python3
 import rospy
 import actionlib
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseActionFeedback
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from geometry_msgs.msg import PoseStamped
 
 
 class MovebaseController:
+    # Initializes the MovebaseController
     def __init__(self):
         rospy.loginfo("[MoveBaseController] Initializing action client")
         self.client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
@@ -17,9 +18,14 @@ class MovebaseController:
         rospy.loginfo("[MoveBaseController] Initializing action goal")
         self.duration = 30
 
+    # The callback of the feedback function which prints the results as debug print.
     def feedback_callback(self, feedback):
         rospy.logdebug(feedback)
 
+    # The function which sends the received goal to the move_base action server.
+    # If the goal is reached, which means that the position of the received goal is reached within the time limit, 
+    # the function returns true so that this goal can be deleted from the list and the next goal selected.
+    # But if the goal was not reached the function returns false and the goal will not be deleted and another goal will be selected.
     def move_base(self, goal):
         rospy.loginfo("[MoveBaseController] Initializing next navigation point")
         target_pose = PoseStamped()
@@ -52,20 +58,15 @@ class MovebaseController:
             rospy.logwarn("[MovebaseController] Action failed: Robot couldn't reach the goal point")
             return False
 
-
+    # This function has the task of navigating the robot to the helper goal for the maneuvering of the robot into the hard zone.
     def move_into_hard_zone(self):
         rospy.loginfo("[MoveBaseController] Initializing next navigation point")
         target_pose = PoseStamped()
 
-        x_new = 1.24827112437622
-        y_new = -0.66988540757462
-        x_old = 1.8000541357545716
-        y_old = -0.44184322825998795
-
         target_pose.header.frame_id = "map"
         target_pose.header.stamp = rospy.Time.now()
-        target_pose.pose.position.x = x_new
-        target_pose.pose.position.y = y_new
+        target_pose.pose.position.x = 1.24827112437622
+        target_pose.pose.position.y = -0.66988540757462
         target_pose.pose.position.z = 0.0
         target_pose.pose.orientation.x = 0.0
         target_pose.pose.orientation.y = 0.0
