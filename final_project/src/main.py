bdf0287fa380cbb4b95af3ddb0ddbee871f4348b
fs-213804 <fs-213804@rwu.de>
2023-11-22 00:22:01 +0100

    added comments to main and removed unused code #29

--- final_project/src/main.py
+++ final_project/src/main.py
@@ -5,6 +5,7 @@ import manual_control
 
 
 class Main:
+    # Initializes the main class
     def __init__(self):
         rospy.loginfo(
             "[Main] Initializing ROS Node"
@@ -13,18 +14,19 @@ class Main:
         self.goals_list = points.GoalsList()
         self.manual_control = manual_control.ManualControl()
 
+    # This function is the main function for the robot operations.
     def operate_robot(self):
         self.goals_list.read_goals()
         rospy.loginfo("[Main] ---------------------")
 
-        duration = rospy.Duration(2)
+        duration = rospy.Duration(3)
         rospy.sleep(duration)
 
         self.goals_list.navigating_easy_zone()
-        self.manual_control.enter_hard_zone(self.goals_list.get_point_four(), self.goals_list.get_point_six())        
+        self.manual_control.enter_hard_zone(self.goals_list.get_point_six())        
         self.goals_list.navigating_hard_zone()
 
-
+# Creates an instance of Main and calls the operate_robot() function.
 if __name__ == "__main__":
     main = Main()
     main.operate_robot()
