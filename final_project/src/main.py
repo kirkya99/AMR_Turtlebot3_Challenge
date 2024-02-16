b2f9bf63eddd47adf3a125543b76c8901cd2ceeb
kirkya99 <fabianschotte@outlook.com>
2024-02-16 09:02:00 +0100

    commented move through wall opening function because of high collision chance

--- final_project/src/main.py
+++ final_project/src/main.py
@@ -21,7 +21,7 @@ class Main:
         rospy.sleep(duration)
 
         self.goals_list.navigating_easy_zone()
-        self.manual_control.enter_hard_zone(self.goals_list.get_point_four(), self.goals_list.get_point_six())        
+        # self.manual_control.enter_hard_zone(self.goals_list.get_point_four(), self.goals_list.get_point_six())        
         self.goals_list.navigating_hard_zone()
 
 
